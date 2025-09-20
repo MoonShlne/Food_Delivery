8961b48c0fc77eb50fd9d594c4d87cecdb59cc3c
MoonShlne <1747874921a@gmail.com>
2025-09-20 15:09:31 +0800

    新增员工业务代码

--- foodDelivery/sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ foodDelivery/sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,7 @@
 package com.sky.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +17,7 @@ import java.time.LocalDateTime;
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     private String username;
--- foodDelivery/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ foodDelivery/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,11 @@
 package com.sky.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +13,13 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.apache.tomcat.util.security.MD5Encoder;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -28,9 +36,14 @@ public class EmployeeServiceImpl implements EmployeeService {
     public Employee login(EmployeeLoginDTO employeeLoginDTO) {
         String username = employeeLoginDTO.getUsername();
         String password = employeeLoginDTO.getPassword();
+        //使用mybatis plus 的lambda query wrapper
+        LambdaQueryWrapper<Employee> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Employee::getUsername,username);
+        Employee employee = employeeMapper.selectOne(wrapper);
 
-        //1、根据用户名查询数据库中的数据
-        Employee employee = employeeMapper.getByUsername(username);
+
+//        //1、根据用户名查询数据库中的数据
+//        Employee employee = employeeMapper.getByUsername(username);
 
         //2、处理各种异常情况（用户名不存在、密码不对、账号被锁定）
         if (employee == null) {
@@ -38,9 +51,11 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
+
         //密码比对
         // TODO 后期需要进行md5加密，然后再进行比对
-        if (!password.equals(employee.getPassword())) {
+        String encPassword = DigestUtils.md5DigestAsHex(password.getBytes());
+        if (!encPassword.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
@@ -52,6 +67,34 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        //把传来的属性复制给employee
+        BeanUtils.copyProperties(employeeDTO, employee);
+        //给其他空属性赋值
+        //设置账号的状态
+        employee.setStatus(StatusConstant.ENABLE);
+        //创建密码
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        //创建时间
+        employee.setCreateTime(LocalDateTime.now());
+        //更新时间
+        employee.setUpdateTime(LocalDateTime.now());
+        //createUser
+        employee.setCreateUser(BaseContext.getCurrentId());
+        //updateUser
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+
+        employeeMapper.insert(employee);
     }
 
 }
