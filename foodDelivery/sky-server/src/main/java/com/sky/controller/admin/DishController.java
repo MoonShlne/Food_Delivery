9114597770d52094f175d3757150c5971683b045
MoonShlne <1747874921a@gmail.com>
2025-09-27 18:26:26 +0800

    feat: Stop tracking application config files &&DI

--- foodDelivery/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ foodDelivery/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -27,7 +27,6 @@ public class DishController {
     private DishService dishService;
 
 
-
     @PostMapping
     @ApiOperation(value = "新增菜品")
     public Result save(@RequestBody DishDTO dishDTO) {
